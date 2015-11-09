f3e57678ce127b2757278c274e1bba601b64b62c
Daniel Pollock <daniellpollock@gmail.com>
2015-11-09 11:15:30 -0600

    Added a car class

---
+++ GitHubTest/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubTest
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Year { get; set; }
+        public string Model { get; set; }
+    }
+}
